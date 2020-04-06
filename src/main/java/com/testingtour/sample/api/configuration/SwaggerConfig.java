3e61abdcd81e40ffd7be5fff58df279406888a8f
hpimentel <me@hirzamitz.com>
2020-04-05 22:13:01 -0400

    update SwaggerConfig.java

--- src/main/java/com/testingtour/sample/api/configuration/SwaggerConfig.java
+++ src/main/java/com/testingtour/sample/api/configuration/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.testingtour.sample.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -19,6 +20,15 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
             .apis(RequestHandlerSelectors.basePackage("com.testingtour.sample.api.controller"))
             .build();
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
